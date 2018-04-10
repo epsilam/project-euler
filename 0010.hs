7bbeaa0c45bbd9bb5201906d38a7d7ab0b7c65d4
lambda-epsilon <38050380+lambda-epsilon@users.noreply.github.com>
2018-04-10 20:07:17 +0300

    Delete 0010.hs

--- 0010.hs
+++
@@ -1,7 +0,0 @@
-main = print $ primeSum 2000000
-
-primeSum n = sum (primes n) where
-    primes n = 2 : 3 : sieve (filter (\x -> not (mod x 3 == 0)) [5,7 .. n]) where
-        sieve (p:xs)
-            | length (p:xs) > 1 = p : sieve [x | x <- xs, mod x p /= 0]
-            | length (p:xs) == 1 = [p]
