738fa0f7291f42afce1e679d950b4a0a5dfdfb54
epsilam <38050380+epsilam@users.noreply.github.com>
2018-04-10 20:13:17 +0300

    removed files

--- 0025.hs
+++
@@ -1,5 +0,0 @@
-main = print $ length $ fibList [1,1]
-    where
-        fibList (x:y:xs)
-            | (length $ show x) == 1000 = x:y:xs
-            | otherwise = fibList ((x+y):x:y:xs)
--- 0026.hs
+++
@@ -1,1 +0,0 @@
--- Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
